66244d6138611a7b7c26b21aa6e38e69d95c90cb
Lukas Valta <valtalukas@seznam.cz>
2021-08-06 15:00:05 +0200

    kurva

--- WithLayout.tsx
+++ WithLayout.tsx
@@ -1,21 +1,45 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { ThemeProvider } from "@material-ui/core/styles"
 import { Paper } from "@material-ui/core"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import getTheme from "./src/theme/index"
 import { Helmet } from "react-helmet"
 import AOS from "aos"
-import "aos/dist/aos.css"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
 import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    width: "90vh",
-  },
+  root: {},
 }))
+
+export const useDarkMode = () => {
+  const [themeMode, setTheme] = useState("light")
+  const [mountedComponent, setMountedComponent] = useState(false)
+
+  const setMode = mode => {
+    window.localStorage.setItem("themeMode", mode)
+    setTheme(mode)
+  }
+
+  const themeToggler = () => {
+    themeMode === "light" ? setMode("light") : setMode("light")
+  }
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("themeMode")
+    localTheme ? setTheme(localTheme) : setMode("light")
+    setMountedComponent(true)
+    AOS.refresh()
+  }, [])
+
+  useEffect(() => {
+    AOS.refresh()
+  }, [themeMode])
+
+  return [themeMode, themeToggler, mountedComponent]
+}
 
 interface Props {
   layout: any
@@ -43,12 +67,15 @@ export default function WithLayout({
       easing: "ease-in-out",
     })
   }, [])
+
+  const [themeMode, themeToggler, mountedComponent] = useDarkMode()
+  useEffect(() => {
+    AOS.refresh()
+  }, [mountedComponent])
   const classes = useStyles()
-  /*  useEffect(() => {
-    AOS.init()
-  }, []) */
   return (
-    <ThemeProvider theme={getTheme("light")}>
+    <ThemeProvider theme={getTheme(themeMode)}>
+      <CssBaseline />
       <Helmet>
         <script
           src="https://kit.fontawesome.com/4c273e6d43.js"
@@ -56,10 +83,10 @@ export default function WithLayout({
         ></script>
       </Helmet>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
+
       <Paper elevation={0}>
-        <Layout>
-          <Component themeMode={"light"} {...rest} />
+        <Layout className={classes.root}>
+          <Component themeMode={themeMode} {...rest} />
         </Layout>
       </Paper>
     </ThemeProvider>
