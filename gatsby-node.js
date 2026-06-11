6989feff6619bcf1bb4067e424c31bff7c7a523f
valta-studio <valtastudio.app@gmail.com>
2026-06-11 10:57:09 +0200

    fix build

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,14 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  createTypes(`
+    type ContentfulNabizim implements Node {
+      documents: [ContentfulAsset] @link(from: "documents___NODE")
+    }
+  `)
+}
 
 /* const path = require("path")
 
